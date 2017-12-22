b223042b25430137a1d5ed9c2f2250a7513dd07a
yantang579 <liuqingyu579@163.com>
2017-12-23 00:33:45 +0800

    2017.12.23

---
+++ src/main/java/com/leetcode/classes/TreeNode.java
@@ -0,0 +1,11 @@
+package com.leetcode.classes;
+
+/**
+ * Created by yantang on 2017/12/23.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
