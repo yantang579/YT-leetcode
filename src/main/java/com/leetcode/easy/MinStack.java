d62a12d8c4245ef363ae6afe131d143d4aae2640
lqy <liuqingyu579@163.com>
2017-12-27 21:56:42 +0800

    2017.12.27

---
+++ src/main/java/com/leetcode/easy/MinStack.java
@@ -0,0 +1,52 @@
+package com.leetcode.easy;
+
+/**
+ * Created by lqy on 2017/12/27.
+ * <p>
+ * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+ * <p>
+ * push(x) -- Push element x onto stack.
+ * pop() -- Removes the element on top of the stack.
+ * top() -- Get the top element.
+ * getMin() -- Retrieve the minimum element in the stack.
+ * Example:
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> Returns -3.
+ * minStack.pop();
+ * minStack.top();      --> Returns 0.
+ * minStack.getMin();   --> Returns -2.
+ */
+public class MinStack {
+
+    /**
+     * Your MinStack object will be instantiated and called as such:
+     * MinStack obj = new MinStack();
+     * obj.push(x);
+     * obj.pop();
+     * int param_3 = obj.top();
+     * int param_4 = obj.getMin();
+     */
+    /** initialize your data structure here. */
+    public MinStack() {
+
+    }
+
+    public void push(int x) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+
+    }
+
+    public int getMin() {
+
+    }
+}
---
+++ src/test/java/com/leetcode/Test_BianryWatch.java
@@ -0,0 +1,12 @@
+package com.leetcode;
+
+import com.leetcode.easy.BinaryWatch;
+/**
+ * Created by lqy on 2017/12/27.
+ */
+public class Test_BianryWatch {
+    public static void main(String[] args){
+        BinaryWatch test= new BinaryWatch();
+        test.readBinaryWatch2(0);
+    }
+}
