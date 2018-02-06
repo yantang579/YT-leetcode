01e6fa3bda8f4a9d4fbe96272670656b76a23991
yantang579 <liuqingyu579@163.com>
2018-02-06 21:58:15 +0800

    2018.02.26

--- src/main/java/com/leetcode/easy/ReverseBits.java
+++ src/main/java/com/leetcode/easy/ReverseBits.java
@@ -10,22 +10,26 @@ package com.leetcode.easy;
  * If this function is called many times, how would you optimize it?
  */
 public class ReverseBits {
+
     // you need treat n as an unsigned value
+
+    /**
+     * 设这个数为n，用一个初值为0的数result保存反转后的结果
+     * 用1对n进行求与，其结果与result进行相加，
+     * 再对n向右进行一位移位，对result向左进行一位移位
+     * 直到n的最后一位处理完。
+     * @param n
+     * @return
+     */
     public int reverseBits(int n) {
-        StringBuilder str=new StringBuilder();
-        while(n!=0){
-            str.append(n%2);
-            n=n/2;
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result += n & 1;
+            n>>>=1;
+            if(i<31) {
+                result<<=1;
+            }
         }
-        while(str.length()<32){
-            str.append(0);
-        }
-        int res=0;
-        int factor=1;
-        for(int i=str.length()-1;i>=0;i--){
-            res+=str.charAt(i)*factor;
-            factor*=2;
-        }
-        return res;
+        return result;
     }
 }
