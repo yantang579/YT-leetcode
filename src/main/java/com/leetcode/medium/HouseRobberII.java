3073514092ac0462ea6d958f08128418b65e666a
lqy <liuqingyu579@163.com>
2018-05-02 21:39:20 +0800

    2018.5.2

---
+++ src/main/java/com/leetcode/medium/HouseRobberII.java
@@ -0,0 +1,27 @@
+package com.leetcode.medium;
+
+/**
+ * Created by lqy on 2018/5/2.
+ * <p>
+ * You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+ * <p>
+ * Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [2,3,2]
+ * Output: 3
+ * Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2),
+ * because they are adjacent houses.
+ * Example 2:
+ * <p>
+ * Input: [1,2,3,1]
+ * Output: 4
+ * Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+ * Total amount you can rob = 1 + 3 = 4.
+ */
+public class HouseRobberII {
+    public int rob(int[] nums) {
+
+    }
+}
